713711989e4047aed441a4e254d9d7f26686eadc
thompskr51 <141582042+thompskr51@users.noreply.github.com>
2023-08-14 19:30:28 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+/* let getComputerChoice = function () {
+    console.log(Math.random().Math.floor(2));
+};
+if (getComputerChoice === 0){
+    console.log('rock');
+} else if (getComputerChoice === 1) {
+    console.log('paper');
+} else if (getComputerChoice === 2) {
+    console.log('scissors');
+}
+getComputerChoice(1)
+ */
+
+/* let getUserChoice = function (userInput) {
+    console.log(userInput.toLowerCase());
+}; if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' */){
+   /*  return userInput;
+} else {
+    console.log('Error');
+}
+getUserChoice ('paper'); */
