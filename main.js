0da47fc8f9e8f4754434e59e6aca9e2fecfb247a
thompskr51 <141582042+thompskr51@users.noreply.github.com>
2023-08-24 11:00:40 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,57 @@
-/* let getComputerChoice = function () {
-    console.log(Math.random().Math.floor(2));
+const getUserChoice = function (userInput) {
+  userInput = userInput.toLowerCase();
+  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+    console.log(userInput);
+  } else {
+  console.log('Invalid selection!');
+  }
 };
-if (getComputerChoice === 0){
-    console.log('rock');
-} else if (getComputerChoice === 1) {
-    console.log('paper');
-} else if (getComputerChoice === 2) {
-    console.log('scissors');
-}
-getComputerChoice(1)
- */
 
-/* let getUserChoice = function (userInput) {
-    console.log(userInput.toLowerCase());
-}; if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' */){
-   /*  return userInput;
-} else {
-    console.log('Error');
+const getComputerChoice = function () {
+  let randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    console.log('paper');
+  } else if (randomNumber === 1) {
+    console.log('rock');
+  } else if (randomNumber === 2) {
+    console.log('scissors');
+  }
+    };
+
+const determineWinner = function (userChoice, computerChoice) {
+if (userChoice === computerChoice) {
+console.log('Tie!')
 }
-getUserChoice ('paper'); */
+if (userChoice === 'rock') {
+  if (computerChoice === 'paper') {
+    console.log('Computer wins!');
+  } else {
+    console.log('User wins!');
+  }
+}
+
+  if (userChoice === 'paper') {
+  if (computerChoice === 'scissors') {
+    console.log('Computer wins!');
+  } else {
+    console.log('User wins!');
+  }
+}
+
+  if (userChoice === 'scissors') {
+   if (computerChoice === 'rock') {
+     console.log('Computer wins!');
+   } else {
+     console.log('User wins!');
+   }
+ }
+};
+
+var playGame = function () {
+  var userChoice = getUserChoice('rock');
+  var computerChoice = getComputerChoice();
+  console.log(`User threw ${userChoice}!`);
+  console.log(`Computer threw ${computerChoice}!`);
+};
+
+  playGame(determineWinner (userChoice, computerChoice));
